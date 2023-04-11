3603b458217aee03679d32b5512f37b781f34de0
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-11 10:30:02 -0400

    Event handling

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: "Client-ID AvoBsZgZKtr5629eFbvJe3EbcGvi2b2btkycCDI6ZMc"
         },
         params: {
-            query: 'cars'
+            query: term,
         },
     });
-    console.log(response);
 
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import searchImages from './api';
 
-searchImages();
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
