68853d0b69e365e246cea91d3851bd6960caa2ca
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-11 11:30:25 -0400

    Css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
 import ImageList from './components/ImageList';
 import searchImages from "./api";
+import './App.css'
 
 function App() {
     const [images, setImages] = useState([])
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,6 +17,7 @@ function SearchBar({ onSubmit }) {
     return (
         <div className='search-bar'>
             <form onSubmit={handleFormSubmit}>
+                <label>Enter Search Term:</label>
                 <input value={term} onChange={handleChange} />
             </form>
         </div>
